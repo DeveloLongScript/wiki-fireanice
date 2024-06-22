2b01fbbba8105de3b1519635e95f16054ebc61a3
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2024-06-21 20:39:57 -0500

    Bump 1.5.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -154,7 +154,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <ThemeToggle className="m-2" />
             <strong className="text-2xl p-4">FireAnIceBox Wiki</strong>
             <div className="items-end flex">
-              <span className="font-normal max-lg:hidden">version 0.5.0</span>
+              <span className="font-normal max-lg:hidden">version 0.5.1</span>
             </div>
           </div>
           <ResizablePanelGroup direction="horizontal">
