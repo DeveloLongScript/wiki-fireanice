714f28d30643926f43b775d3e6c83942c16eae05
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2024-06-23 21:19:05 -0500

    ....

--- src/pages/api/inngest.ts
+++ src/pages/api/inngest.ts
@@ -80,6 +80,7 @@ export default serve({
                   "User-Agent":
                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
                 },
+                mode: "cors",
                 method: "GET",
               })
             ).text(),
